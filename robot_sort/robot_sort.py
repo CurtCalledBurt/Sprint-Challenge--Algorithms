929acf1e4d87c4ef27f8138c196f2494167baf6c
CurtCalledBurt <CurtCalledBurt@users.noreply.github.com>
2020-03-20 10:29:17 -0700

    implemented robot_sort. Robot uses bubblesort.

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -96,15 +96,49 @@ class SortingRobot:
         """
         Sort the robot's list.
         """
-        # Fill this out
-        pass
+        # #bubblesort
+        # while the light is on, keep working
+        self.set_light_on()
+        while self.light_is_on():
+            # turn light off, turn it on again if we ever make a switch
+            self.set_light_off()
+
+            # return to beginning of the list
+            while self.can_move_left():
+                self.move_left()
+
+            # swap, move, compare then swap, move, swap, move loop
+            while self.can_move_right():
+                # swap the None we are holding for the item we are currenlty in front of
+                self.swap_item()
+                # move to next item
+                self.move_right()
+                # compare the item we are holding with item in front of us. 
+                # If what we are holding is bigger ( 1 ), swap them 
+                # (we are always holding the bigger one after this step)
+                if self.compare_item() > 0:
+                    self.swap_item()
+                    # Turn on our light when we make a swap, 
+                    # this signals that the list was still out of order, 
+                    # so we'll need to loop through again
+                    self.set_light_on()
+                # move left and pick up None object again, 
+                # leaving the two compared elements in the correct 
+                # positions with respect to eachother
+                self.move_left()
+                self.swap_item()
+                # move right so we're infront of the bigger item of the two we just compared
+                self.move_right()
+
+        return
 
 
 if __name__ == "__main__":
     # Test our your implementation from the command line
     # with `python robot_sort.py`
 
-    l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1, 45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
+    # put in an extra 46 to see how the robot handled like numbers.
+    l = [15, 41, 58,       46,        49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1, 45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
 
     robot = SortingRobot(l)
 
